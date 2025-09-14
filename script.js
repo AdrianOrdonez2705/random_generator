c1ce50fbf2c8f875f8947d98631421a7c27f6d3d
AdrianOrdonez2705 <fabriadri2705@gmail.com>
2025-09-13 22:23:44 -0400

    Implementación algoritmo congruencial multiplicativo

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const notXo = document.getElementById("Xo");
 const notK = document.getElementById("k");
 const notC = document.getElementById("c");
 
+let linear = true;
+let uno = true;
+
 let P, Xo, k, c;
 let a, g, m;
+let algorithm = 'lineal';
 
 const results = document.getElementById("results");
 const generar = document.getElementById("generar");
 const limpiar = document.getElementById("limpiar");
+const algoSubtitle = document.getElementById("algoSubtitle");
+const formulas = document.getElementById("formulas");
 
 limpiar.addEventListener('click', () => {
     notP.value = '';
@@ -17,6 +23,39 @@ limpiar.addEventListener('click', () => {
     notC.value = '';
     results.innerHTML = '';
 });
+
+document.querySelectorAll('input[name="algoritmo"]').forEach((radio) => {
+    radio.addEventListener('change', () => {
+        if (document.getElementById('lineal').checked) {
+            algorithm = 'lineal';
+            algoSubtitle.textContent = 'Algoritmo Lineal';
+            formulas.style.display = 'none';
+            linear = true;
+            notC.style.display = 'inline-block';
+        } else if (document.getElementById('multiplicativo').checked) {
+            algorithm = 'multiplicativo';
+            algoSubtitle.textContent = 'Algoritmo Multiplicativo';
+            formulas.style.display = 'block';
+            linear = false;
+            notC.style.display = 'none';
+        }
+    });
+});
+
+document.querySelectorAll('input[name="formula"]').forEach((radio) => {
+    radio.addEventListener('change', () => {
+        if (document.getElementById('uno').checked) {
+            algorithm = 'multiplicativo';
+            linear = false;
+            uno = true;
+        } else if (document.getElementById('dos').checked) {
+            algorithm = 'multiplicativo';
+            linear = false;
+            uno = false;
+        }
+    });
+});
+
 
 generar.addEventListener('click', () => {
     try {
@@ -31,25 +70,38 @@ generar.addEventListener('click', () => {
         P = parseFloat(notP.value);
         Xo = parseFloat(notXo.value);
         k = parseFloat(notK.value);
-        c = parseFloat(notC.value);
-
-        if (isNaN(P) || isNaN(Xo) || isNaN(k) || isNaN(c)) {
+        
+        if (isNaN(P) || isNaN(Xo) || isNaN(k)) {
             throw new Error("Entrada inválida");
         }
 
-        if (!esEntero(P) || !esEntero(Xo) || !esEntero(k) || !esEntero(c)) {
+        if (!esEntero(P) || !esEntero(Xo) || !esEntero(k)) {
             throw new Error("Los valores deben ser números enteros positivos")
         }
 
-        if (!esPrimo(c)) {
-            throw new Error("c debe ser primo");
+        if (linear === true) {
+            c = parseFloat(notC.value);
+            if (!esPrimo(c) || isNaN(c) || !esEntero(c)) {
+                throw new Error("c debe ser primo, positivo y entero");
+            }
+
+            a = 1 + (4 * k);
+            g = Math.log(P) / Math.log(2);
+            m = Math.pow(2, g);
+            calculateResultsLinear();
         }
 
-        a = 1 + (4 * k);
-        g = Math.log(P) / Math.log(2);
-        m = Math.pow(2, g);
+        if (linear === false) {
+            if (uno === true) {
+                a = 3 + (8 * k);
+            } else if (uno === false) {
+                a = 5 + (8 * k);
+            }
 
-        calculateResults();
+            g = (Math.log(P) / Math.log(2)) + 2;
+            m = Math.pow(2, g);
+            calculateResultsMultiplicativo();
+        }
 
     } catch (error) {
         const errorMessage = `
@@ -61,9 +113,27 @@ generar.addEventListener('click', () => {
     }
 });
 
-function calculateResults() {
+function calculateResultsLinear() {
     for (let i = 0; i < P + 1; i++) {
         let x = ((a * Xo) + c) % m;
+        let r = (x / (m - 1)).toFixed(4);
+
+        const tab = `
+            <tr>
+                <td>${i + 1}</td>
+                <td>${x}</td>
+                <td>${r}</td>
+            </tr>
+        `;
+
+        results.innerHTML += tab;
+        Xo = x;
+    }
+}
+
+function calculateResultsMultiplicativo() {
+    for (let i = 0; i < P + 1; i++) {
+        let x = (a * Xo) % m;
         let r = (x / (m - 1)).toFixed(4);
 
         const tab = `
